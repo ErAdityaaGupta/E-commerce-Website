b91f8bf7d01b1d8c03f1956d0dab6c80de363012
Aditya <ingenious.adig@gmail.com>
2025-03-21 15:49:09 +0530

    Deleting Product

--- src/main/java/com/ecom/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/ecom/ecommerce/service/ProductServiceImpl.java
@@ -102,4 +102,14 @@ public class ProductServiceImpl implements ProductService {
 
         return modelMapper.map(savedProduct, ProductDTO.class);
     }
+
+    @Override
+    public ProductDTO deleteProduct(Long productId) {
+
+        Product productFromDb = productRepo.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product","productId",productId));
+
+        productRepo.delete(productFromDb);
+        return modelMapper.map(productFromDb, ProductDTO.class);
+    }
 }
