e627f6c071da7c067a8746c23b97ca8d0ef92700
Aditya <ingenious.adig@gmail.com>
2025-03-23 00:17:00 +0530

    Updating the Product Image

--- src/main/java/com/ecom/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/ecom/ecommerce/service/ProductServiceImpl.java
@@ -9,6 +9,7 @@ import com.ecom.ecommerce.repo.CategoryRepo;
 import com.ecom.ecommerce.repo.ProductRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +31,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
@@ -126,8 +133,7 @@ public class ProductServiceImpl implements ProductService {
         Product productFromDb = productRepo.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product","productId",productId));
 
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+        String fileName = fileService.uploadImage(path, image);
 
         productFromDb.setImage(fileName);
 
@@ -136,23 +142,5 @@ public class ProductServiceImpl implements ProductService {
         return modelMapper.map(savedProduct, ProductDTO.class);
     }
 
-    private String uploadImage(String path, MultipartFile file) throws IOException {
 
-        String originalFilename = file.getOriginalFilename();
-
-        String randomId = UUID.randomUUID().toString();
-
-        String fileName = randomId.concat(originalFilename.substring(originalFilename.lastIndexOf(".")));
-
-        String filePath = path + File.separator + fileName;
-
-        File folder = new File(filePath);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-        return fileName;
-    }
 }
